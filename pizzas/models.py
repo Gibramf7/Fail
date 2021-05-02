c32b516ae464b47571bf33c9973e522282c52ae6
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-05-02 01:53:44 -0500

    second commit

--- pizzas/models.py
+++ pizzas/models.py
@@ -16,7 +16,7 @@ class Pizza(models.Model):
 class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.TextField()
-   # date_added = models.DateTimeField(auto_now_add=True)
+    #date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    '''The home page for Pizzeria. '''
+    return render(request, 'pizzas/index.html')
+
+
+def pizzas(request):
+    pizzas = Pizza.objects
+
+    context = {'pizzas':pizzas}
+
+    return render(request, 'pizzas/pizzas.html', context)
